ea2e5de63d56bd82a21594b1dbfb8d45dddf35a6
TAlderman1 <52610092+TAlderman1@users.noreply.github.com>
2020-09-05 02:31:44 -0400

    Created substrings.rb file

---
+++ substrings.rb
@@ -0,0 +1,16 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(str, arr)
+  countHash = Hash.new(0)
+  strSplit = str.downcase.split(" ")
+  strSplit.each() do |e|
+    arr.each() do |r|
+      if e.include?(r)
+        countHash[r] += 1
+      end
+    end
+  end
+  pp countHash
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
